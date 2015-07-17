987419b0608b6687d390c6e97a492163108af53e
Kenny <forkburn@gmail.com>
2015-07-17 19:22:25 +0800

    restructured modules added numberpicker pref module

--- app/src/main/java/com/greenpudding/activities/MainActivity.java
+++ app/src/main/java/com/greenpudding/activities/MainActivity.java
@@ -1,8 +1,6 @@
 package com.greenpudding.activities;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -15,7 +13,6 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
