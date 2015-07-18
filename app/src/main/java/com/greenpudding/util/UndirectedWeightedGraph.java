16fc805ed9d65d3fbd29368095b7a144e4e00f5d
Kenny <forkburn@gmail.com>
2015-07-18 17:30:37 +0800

    optimize performance

--- app/src/main/java/com/greenpudding/util/UndirectedWeightedGraph.java
+++ app/src/main/java/com/greenpudding/util/UndirectedWeightedGraph.java
@@ -1,37 +1,22 @@
 package com.greenpudding.util;
 
 
-import android.util.Pair;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class UndirectedWeightedGraph {
 
-    private Map<Pair<Integer, Integer>, Double> edgeWeight = new HashMap<>();
+	private double[][] edgeWeight;
 
+	public UndirectedWeightedGraph() {
 
-    public void setEdgeWeight(int id1, int id2, double value) {
-        if (id1 > id2) {
-            edgeWeight.put(new Pair(id1, id2), value);
-        } else {
-            edgeWeight.put(new Pair(id2, id1), value);
+		edgeWeight = new double[500][500];
+	}
 
-        }
-    }
+	public double getEdgeWeight(int id1, int id2) {
+		return edgeWeight[id1][id2];
+	}
 
-    public double getEdgeWeight(int id1, int id2) {
-        try {
-            if (id1 > id2) {
-                return edgeWeight.get(new Pair(id1, id2));
-            } else {
-                return edgeWeight.get(new Pair(id2, id1));
-
-            }
-        }catch(Exception e) {
-            return 0;
-        }
-
-    }
+	public void setEdgeWeight(int id1, int id2, double value) {
+		edgeWeight[id1][id2] = value;
+		edgeWeight[id2][id1] = value;
+	}
 
 }
